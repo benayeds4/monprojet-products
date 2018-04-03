9c8280217510e5e3d56edd0975604b7698c7895d
BEN-AYED-S <ZVEN-DEN@gmail.com>
2018-04-03 11:26:31 +0200

    Commit Secodaire

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -18,8 +18,8 @@ export class ProductListComponent implements OnInit {
 
     {code: 'P100', libelle: 'cafe', prixUnitaire: 500 },
     {code: 'P200', libelle: 'The', prixUnitaire: 300 },
-    {code: 'P300', libelle: 'Jus orange', prixUnitaire: 700 }
-
+    {code: 'P300', libelle: 'Jus orange', prixUnitaire: 700 },
+    {code: 'P400', libelle: 'SHIPS', prixUnitaire: 800 }
   ];
 
 
