60931adc023ae91799ae2f52e1946945fcb7e678
BEN-AYED-S <benayeds4@gmail.com>
2018-04-04 15:21:32 +0200

    new Version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+import {RouterModule ,Routes} from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductService } from './services/product.service';
 import { HttpClientModule } from '@angular/common/http';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
+const appRoutes: Routes = [
+
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'list', component: ProductListComponent },
+  { path: '', component: WelcomeComponent , pathMatch: 'full'},
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProductListComponent,
-    FooterComponent
+    FooterComponent,
+    NotFoundComponent,
+    WelcomeComponent
+
 
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
   ProductService
